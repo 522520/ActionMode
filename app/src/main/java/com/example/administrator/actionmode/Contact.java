999814268af18c3df083b5b1d2d36e97abd98015
Yan <1543045635@qq.com>
2018-04-17 22:54:48 +0800

    Initial commit

---
+++ app/src/main/java/com/example/administrator/actionmode/Contact.java
@@ -0,0 +1,28 @@
+package com.example.administrator.actionmode;
+
+
+public class Contact {
+    private  String userName;
+    private String phoneNume;
+
+    public Contact(String userName, String phoneNume) {
+        this.userName = userName;
+        this.phoneNume = phoneNume;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNume;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public void setPhoneNumber(String phoneNume) {
+        this.phoneNume = phoneNume;
+    }
+}
